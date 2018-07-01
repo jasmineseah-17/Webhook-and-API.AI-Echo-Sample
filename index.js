1eda1b96485eb3d2510e1f21a113d925f28a5e66
jasmineseah-17 <seah.yongyee.jasmine@dhs.sg>
2018-07-01 10:13:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    displayText: speech,
+    fulfillmentMessages: speech,
     source: "webhook-echo-sample"
   });
 });
